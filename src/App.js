3a77f6d1d66dc030ab4eedf7de6ff467e4442609
ladeveloper <ladeveloper99@gmail.com>
2022-04-18 11:33:21 -0700

    fix corrections

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Counter from "./Counter";
+import ReducerTutorial from "./ReducerTutotrial";
+import UseEffectTutorial from "./UseEffectTutorial";
+import UseRef from "./UseRef";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Pedro tech Hooks tutorial</h1>
+      {/* <ReducerTutorial /> */}
+      {/* <UseEffectTutorial /> */}
+      <a href="file.pdf" download="resume.pdf">
+        Download PDF
+      </a>
+
+      <UseRef />
+
+      <Counter />
     </div>
   );
 }
